be8e51f64ccb7076e0651ea213092ffcd01af7ed
Mark Flanagan <markfl@gmail.com>
2026-04-10 08:15:22 +0100

    Remove razor runtime compilation

--- samples/AspNetSimple/Startup.cs
+++ samples/AspNetSimple/Startup.cs
@@ -18,8 +18,8 @@ public class Startup
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-        services.AddControllersWithViews().AddRazorRuntimeCompilation();
-        services.AddRazorPages().AddRazorRuntimeCompilation();
+        services.AddControllersWithViews();
+        services.AddRazorPages();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
